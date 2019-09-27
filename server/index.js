3d14db39d621ddeb233dc3a7293e708f99815cd6
Nathan Lyle Black <nblack@algorithmia.io>
2019-09-27 09:30:42 -0700

    UX-1658 Handle 404 for root path

--- server/index.js
+++ server/index.js
@@ -35,7 +35,7 @@ app.use((req, res, next) => {
 
 // Remove trailing slashes
 app.get('*', (req, res, next) => {
-  if (req.path.endsWith('/')) {
+  if (/.+\/$/.test(req.path)) {
     res.redirect(req.path.replace(/\/$/, ''))
   } else {
     next()
