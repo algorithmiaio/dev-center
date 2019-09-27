a1e81d1a59e3e5263a7a978b3c6044596666e544
Nathan Lyle Black <nblack@algorithmia.io>
2019-09-27 10:32:25 -0700

    UX-1658 Fix messaging

--- server/index.js
+++ server/index.js
@@ -77,7 +77,7 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000
 const server = app.listen(PORT, () => {
-    log.info(`Server server on port ${PORT}.`)
+    log.info(`Server started on port ${PORT}.`)
   }
 )
 
