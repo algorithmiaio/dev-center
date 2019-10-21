b6b2b49493250c2a6355feaf500b033e6655c9c6
Nathan Lyle Black <nblack@algorithmia.io>
2019-10-21 12:12:24 -0700

    Add environment info to Sentry init

--- js/sentry.init.js
+++ js/sentry.init.js
@@ -1,5 +1,6 @@
 window.Sentry && Sentry.init({
   dsn: 'https://f918b2f372cb41018233ed3cd8e3fa37@sentry.io/1788379',
+  environment: window.location.hostname,
   beforeBreadcrumb: (breadcrumb) => {
     const { category, data } = breadcrumb
 
