5f4e2a06ebde230da5647062eaf33778738f87dd
Nathan Lyle Black <nblack@algorithmia.io>
2019-10-21 11:14:58 -0700

    FRONTEND-1660 Add Sentry to docs app

---
+++ js/sentry.init.js
@@ -0,0 +1,12 @@
+window.Sentry && Sentry.init({
+  dsn: 'https://f918b2f372cb41018233ed3cd8e3fa37@sentry.io/1788379',
+  beforeBreadcrumb: (breadcrumb) => {
+    const { category, data } = breadcrumb
+
+    if (category === 'xhr') {
+      data.url = '[REDACTED]'
+    }
+
+    return breadcrumb
+  },
+});
