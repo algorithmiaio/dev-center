08fac446e1fc516c3ad7c90f4cc209a420ca625a
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-01 17:00:12 -0700

    FRONTEND-1867 Add test vue component to dev-center.

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path')
+const webpack = require('webpack')
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            scss: ['vue-style-loader', 'css-loader', 'sass-loader']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue$: 'vue/dist/vue.esm.js'
+    },
+    extensions: ['*', '.js', '.vue', '.json']
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+  ]
+}
+
+if (process.env.NODE_ENV === 'production') {
+  module.exports.mode = 'production'
+}
