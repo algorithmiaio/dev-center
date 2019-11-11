09fef62a7cb5f0d427c78d77dcd09772be48de41
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-11 11:41:21 -0800

    FRONTEND-1900 Add redirects for authentication page.

--- js/api-docs-redirect.js
+++ js/api-docs-redirect.js
@@ -1,7 +1,15 @@
 ;(function redirectApiDocsHash() {
-  if (window.location.pathname.match(/^\/developers\/api\/?$/) && window.location.hash) {
-    switch(window.location.hash.toLowerCase()) {
-      case '#introduction': window.location.assign('/developers/api')
+  function getRedirectRoute(hash) {
+    switch(hash) {
+      case '#introduction': return '/developers/api'
+      case '#authentication': return '/developers/api/authentication'
+      case '#api-keys':
+      case '#key-restrictions': return `/developers/api/authentication${hash}`
+      default: return `/developers/api${hash}`
     }
+  }
+
+  if (window.location.pathname.match(/^\/developers\/api\/?$/) && window.location.hash) {
+    window.location.assign(getRedirectRoute(window.location.hash))
   }
 })()
