15c6d74107de80379e8ec19805fb7a5620b863e5
Nathan Lyle Black <nblack@algorithmia.io>
2019-11-12 15:03:44 -0800

    FRONTEND-1897 Move logic of user cred replacement into Vue

---
+++ src/utils/setupPage.js
@@ -0,0 +1,15 @@
+import { replaceUserCreds } from './replaceUserCreds'
+
+function enableNavButtons(authenticated) {
+  const navClass = authenticated ? 'show-auth-nav' : 'show-unauth-nav'
+  document.querySelector('.syn-page-header').classList.add(navClass)
+}
+
+export function setupPage(user) {
+  enableNavButtons(!!user)
+
+  replaceUserCreds({
+    user,
+    nodes: document.querySelectorAll('[replace-user-creds]')
+  })
+}
