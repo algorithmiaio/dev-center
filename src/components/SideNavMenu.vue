577cf83e0847d654a3d3608d106bd7c3629bfd7a
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-14 16:04:37 -0800

    FRONTEND-1999 Add component to handle menu toggling.

---
+++ src/components/SideNavMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div ref="menu">
+    <slot :toggle="toggle" :activeSection="activeSection"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SideNavMenu',
+  data() {
+    return {
+      activeSection: ''
+    }
+  },
+  methods: {
+    toggle(sectionName) {
+      console.log('toggling', sectionName)
+      this.activeSection = this.activeSection === sectionName ? '' : sectionName
+    },
+    getOpenSection() {
+      const openLink = this.$refs.menu.querySelector('a.cloak-open')
+      if (openLink) {
+        this.activeSection = openLink.id.replace('-link', '')
+      }
+    },
+  },
+  mounted() {
+    this.getOpenSection()
+  }
+}
+</script>
