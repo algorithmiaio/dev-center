fd4e85b228ccf399c5c070ade231608df650d306
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-19 14:52:12 -0800

    FRONTEND-1885 Add plugin to generate TOC for a page from h2s.

---
+++ _plugins/page_toc.rb
@@ -0,0 +1,27 @@
+require "nokogiri"
+
+module Jekyll
+  class PageTocTag < Liquid::Tag
+
+    def initialize(tag_name, markdown, options)
+      super
+    end
+
+    def render(context)
+      content = context['page']['content']
+      html = ['<ul class="syn-width-full syn-overflow-hidden-x syn-scrollable-y">']
+      doc = Nokogiri::HTML(content)
+      doc.css('h2').each do |heading|
+        html << "<li class=\"syn-overflow-hidden allow-wrap\">
+          <a class=\"flex-item-no-shrink syn-flex justify-space-between align-center syn-text-secondary syn-caption syn-mb-0\" href=\"\##{heading["id"]}\">
+            #{heading.text}
+          </a>
+        </li>"
+      end
+      html << '</ul>'
+      html.join("\n")
+    end
+  end
+end
+
+Liquid::Template.register_tag('pagetoc', Jekyll::PageTocTag)
