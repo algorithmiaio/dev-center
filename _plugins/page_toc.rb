2cd451b044f4368f57d28b22c03d57b11c92d7b9
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-20 13:23:33 -0800

    FRONTEND-1887 Style toc nav links.

--- _plugins/page_toc.rb
+++ _plugins/page_toc.rb
@@ -9,19 +9,23 @@ module Jekyll
 
     def render(context)
       content = context['page']['content']
-      html = ['<nav v-scrollspy:scrollspy-content class="syn-width-full">']
+      html = ['<nav v-scrollspy:scrollspy-content="{offset: 64}" class="syn-width-full syn-mt-4">']
       html << '<ul class="syn-width-full syn-overflow-hidden-x syn-scrollable-y">'
       doc = Nokogiri::HTML(content)
       doc.css('h2').each do |heading|
-        html << "<li class=\"syn-overflow-hidden allow-wrap\">
-          <a class=\"flex-item-no-shrink syn-flex justify-space-between align-center syn-text-secondary\" href=\"\##{heading["id"]}\">
-            <div class=\"syn-ml-#{context['toc_indent']}\">#{heading.text}</div>
+        html << "<li class=\"toc-nav-li syn-overflow-hidden allow-wrap\">
+          <a class=\"toc-nav-link flex-item-no-shrink syn-flex justify-space-between align-center syn-text-secondary\" href=\"\##{heading["id"]}\">
+            <div class=\"toc-nav-item syn-ml-#{context['toc_indent']}\">#{heading.text}</div>
           </a>
         </li>"
       end
       html << '</ul>'
       html << '</nav>'
-      html.join("\n")
+      if html.length > 4
+        return html.join("\n")
+      else
+        return ""
+      end
     end
   end
 end
