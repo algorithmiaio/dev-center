d944d4574d594f1050c7096be86f6812a869068b
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-20 13:36:31 -0800

    FRONTEND-1887 Move sub nav styles to synapse.

--- _plugins/page_toc.rb
+++ _plugins/page_toc.rb
@@ -13,9 +13,9 @@ module Jekyll
       html << '<ul class="syn-width-full syn-overflow-hidden-x syn-scrollable-y">'
       doc = Nokogiri::HTML(content)
       doc.css('h2').each do |heading|
-        html << "<li class=\"toc-nav-item syn-overflow-hidden allow-wrap\">
-          <a class=\"toc-nav-link flex-item-no-shrink syn-flex justify-space-between align-center syn-text-secondary\" href=\"\##{heading["id"]}\">
-            <div class=\"toc-nav-text syn-ml-#{context['toc_indent']}\">#{heading.text}</div>
+        html << "<li class=\"sub-nav-item syn-overflow-hidden allow-wrap\">
+          <a class=\"sub-nav-link flex-item-no-shrink syn-flex justify-space-between align-center syn-text-secondary\" href=\"\##{heading["id"]}\">
+            <div class=\"sub-nav-text syn-ml-#{context['toc_indent']}\">#{heading.text}</div>
           </a>
         </li>"
       end
