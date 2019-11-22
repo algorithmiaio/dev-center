d2ef32d0c61929bb2ed6d52a1c46704116b6162b
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-22 15:03:49 -0800

    FRONTEND-1969 Add modal to popout image.

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -1,13 +1,26 @@
 <template>
-  <div class="syn-bg-gray">
-    <div ref="image" class="syn-ma-16 syn-image-shadowed">
-      <slot></slot>
+  <div>
+    <div class="syn-bg-gray syn-flex">
+      <div @click="modalOpen = true" ref="image" class="syn-ma-16 syn-image-shadowed">
+        <slot></slot>
+      </div>
     </div>
+    <modal v-model="modalOpen" no-padding hide-close-btn>
+      <slot></slot>
+    </modal>
   </div>
 </template>
 
 <script>
+import Modal from '../../synapse/components/vue/Modal.vue'
+
 export default {
-  name: 'ImageContainer'
+  name: 'ImageContainer',
+  components: { Modal },
+  data() {
+    return {
+      modalOpen: false,
+    }
+  }
 }
 </script>
