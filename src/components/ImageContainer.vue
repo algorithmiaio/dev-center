c8c970abbd06f903fcbbffc77cf47a0d241698d7
Kelsey Langlois <kels.langlois@gmail.com>
2019-11-22 15:32:00 -0800

    FRONTEND-1969 Add caption prop to image component.

--- src/components/ImageContainer.vue
+++ src/components/ImageContainer.vue
@@ -1,8 +1,11 @@
 <template>
   <div>
-    <div class="syn-bg-gray syn-flex">
+    <div class="syn-bg-gray syn-flex direction-column align-center">
       <div @click="modalOpen = true" ref="image" class="syn-ma-16 syn-image-shadowed">
         <slot></slot>
+      </div>
+      <div v-if="caption" class="syn-overline syn-text-center syn-mh-16 syn-mb-16">
+        {{caption}}
       </div>
     </div>
     <modal v-model="modalOpen" no-padding hide-close-btn large>
@@ -18,6 +21,12 @@ import Modal from '../../synapse/components/vue/Modal.vue'
 
 export default {
   name: 'ImageContainer',
+  props: {
+    caption: {
+      type: String,
+      required: false
+    },
+  },
   components: { Modal },
   data() {
     return {
