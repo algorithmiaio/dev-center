ff42a3b603c9af6e0221ef13bb21e776a2204873
Nathan Lyle Black <nblack@algorithmia.io>
2019-12-02 09:05:42 -0800

    Use chunkhash on JS files

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,34 @@
 const path = require('path')
 const webpack = require('webpack')
 const { VueLoaderPlugin } = require('vue-loader')
+const ManifestPlugin = require('webpack-manifest-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, './js'),
-    filename: 'vue-build.js'
+  mode: isProd ? 'production' : 'development',
+  entry: {
+    'vue-build': './src/main.js'
   },
-  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[chunkhash:8].js',
+  },
+  optimization: {
+    splitChunks: {
+      // SplitChunks plugin by default only targets 'async' chunks. (Chunks
+      // fetched client side, not those referenced in HTML). Here we tell
+      // Webpack to pull vendor code out of entry chunks too.
+      chunks: 'initial',
+      cacheGroups: {
+        vendor: {
+          test: /node_modules/,
+          name: 'vendor',
+        }
+      }
+    }
+  },
   module: {
     rules: [
       {
@@ -29,10 +49,8 @@ module.exports = {
     extensions: ['*', '.js', '.vue', '.json']
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new VueLoaderPlugin(),
+    new ManifestPlugin()
   ]
-}
-
-if (process.env.NODE_ENV === 'production') {
-  module.exports.mode = 'production'
 }
