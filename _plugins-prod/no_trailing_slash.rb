b6d6cfb216613a1de9be32023e6a61b25babb32a
Nathan Lyle Black <nblack@algorithmia.io>
2020-01-24 15:44:02 -0800

    UXENG-2188 Use symlinks for plugins

--- _plugins-prod/no_trailing_slash.rb
+++ _plugins-prod/no_trailing_slash.rb
@@ -1,13 +1,1 @@
-require 'uri'
-
-module Jekyll
-  module TrailingSlashFilter
-    def no_trailing_slash(url)
-      uri = URI(url)
-      uri.path = uri.path.chomp('/')
-      uri.to_s
-    end
-  end
-end
-
-Liquid::Template.register_filter(Jekyll::TrailingSlashFilter)
+../plugins/no_trailing_slash.rb
